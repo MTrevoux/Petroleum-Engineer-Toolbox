a4e107d0613e53c2ec5f9619f961ae156b9962b9
Marc Trevoux <69000095+MTrevoux@users.noreply.github.com>
2020-10-04 14:13:25 +0200

    Update gasproperties.py

--- petoolbox/gas/gasproperties.py
+++ petoolbox/gas/gasproperties.py
@@ -14,19 +14,19 @@ def zfactorBeggsBrill(P,T,SG):
 	z factor
 	"""
 
-	Ppc=756.8-131.07*SG-3.6*SG**2
-	Tpc=169.2+349.5*SG-74*SG**2
-	Tr=T/Tpc #rankine
-	Pr=P/Ppc #psia
+	Ppc = 756.8 - 131.07 * SG - 3.6 * SG**2
+	Tpc = 169.2 + 349.5 * SG - 74 * SG**2
+	Tr = T / Tpc #rankine
+	Pr = P / Ppc #psia
 
-	A=1.39*(Tr-0.92)**0.5-0.36*Tr-0.1
-	E=9*(Tr-1)
-	F=0.3106-0.49*Tr+0.1824*Tr**2
-	B=(0.62-0.23*Tr)*Pr+(0.066/(Tr-0.86)-0.037)*Pr**2+0.32*Pr**2/10**E
-	C=0.132-0.32*log10(Tr)
-	D=10**F
+	A = 1.39 * (Tr - 0.92)**0.5 - 0.36 * Tr - 0.1
+	E = 9 * (Tr - 1)
+	F = 0.3106 - 0.49 * Tr + 0.1824 * Tr**2
+	B = (0.62 - 0.23 * Tr) * Pr + (0.066 / (Tr - 0.86) - 0.037) * Pr**2 + 0.32 * Pr**2 / 10**E
+	C = 0.132 - 0.32 * log10(Tr)
+	D = 10**F
 
-	z=A+(1-A)/exp(B)+C*Pr**D
+	z = A + (1 - A) / exp(B) + C * Pr**D
 
 	return z
 
@@ -43,23 +43,25 @@ def zfactorDAK(P,T,SG):
 	OUTPUT
 	[z factor, Boolean]
 	"""
-	Ppc=756.8-131.07*SG-3.6*SG**2
-	Tpc=169.2+349.5*SG-74*SG**2
-	Tr=T/Tpc #rankine
-	Pr=P/Ppc #psia
+	Ppc = 756.8 - 131.07 * SG - 3.6 * SG**2
+	Tpc= 169.2 + 349.5 * SG - 74 * SG**2
+	Tr = T / Tpc #rankine
+	Pr = P / Ppc #psia
 	#Dranchuk / Abou Kassem
 	def katzfunc(z):
-		rhor=(0.27*Pr/(Tr*z))
-		C=[0,0.3265,-1.07,-0.5339,0.01569,-0.05165,0.5475,-0.7361,0.1844,0.1056,0.6134,0.7210]
-		return -z+1+(C[1]+C[2]/Tr+C[3]/Tr**3+C[4]/Tr**4+C[5]/Tr**5)*rhor+(C[6]+C[7]/Tr+C[8]/Tr**2)*rhor**2-C[9]*(C[7]/Tr+C[8]/Tr**2)*rhor**5+C[10]*(1+C[11]*rhor**2)*(rhor**2/Tr**3*exp(-C[11]*rhor**2))
+		rhor = 0.27 * Pr / ( Tr * z)
+		C = [0, 0.3265, -1.07, -0.5339, 0.01569, -0.05165, 0.5475, -0.7361, 0.1844, 0.1056, 0.6134, 0.7210]
+		RESULT = -z + 1
+		+ (C[1] + C[2] / Tr + C[3] / Tr**3 + C[4] / Tr**4 + C[5] / Tr**5) * rhor + (C[6] + C[7] / Tr + C[8] / Tr**2) * rhor**2 - C[9] * (C[7] / Tr + C[8] / Tr**2 ) * rhor**5 + C[10] * (1 + C[11] * rhor**2) * (rhor**2 / Tr**3* exp(-C[11] * rhor**2))
+		return RESULT
 
-	X=root(katzfunc,1)
-	results= [X.x[0],X.success]
+	X = root(katzfunc,1)
+	results = [X.x[0], X.success]
 
 	return results
 
 
-def SGgas(z,P,T,SG):
+def gasSG(z,P,T,SG):
 	"""
 	INPUT
 	P = psia / T = f°
@@ -68,7 +70,7 @@ def SGgas(z,P,T,SG):
 	OUTPUT
 	Gas specific gravity at P and T
 	"""
-	return 28.967*SG*P/(z*(T+459.67)*10.732)/(28.967*14.7/((459.67+60)*10.732))
+	return 28.967 * SG * P / (z * (T + 459.67) * 10.732) / (28.967 * 14.7 / ((459.67 + 60) * 10.732))
 
 def rhoGas(z,P,T,SG):
 	"""
@@ -79,9 +81,9 @@ def rhoGas(z,P,T,SG):
 	OUTPUT
 	Gas gravity in lbs/ft3 at P and T
 	"""
-	return 28.967*SG*P/(z*(T+459.67)*10.732)
+	return 28.967 * SG * P / (z * (T + 459.67) * 10.732)
 
-def volumeStd(V,z,P,T):
+def standardVolume(V,z,P,T):
 	"""
 	Function to calculate a volume into standard conditions.
 	INPUT
@@ -92,7 +94,7 @@ def volumeStd(V,z,P,T):
 	OUTPUT
 	volume in the same unit as V in standard condition 1 atm / 60°F.
 	"""
-	return V*P/(z*(T+459.67))*1/(14.7/(459.67+60))
+	return V * P / (z * (T + 459.67)) * 1/(14.7 / (459.67 + 60))
 
 def fluidPressure(rho,h):
 	"""
@@ -101,5 +103,5 @@ def fluidPressure(rho,h):
 	OUTPUT
 	Pressure at the bottom of fluid column
 	"""
-	return rho*h/144
+	return rho * h / 144
 
