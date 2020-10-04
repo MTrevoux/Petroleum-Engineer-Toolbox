a8db9c25975336802bd506417c22561fa9ecb1d3
Marc Trevoux <69000095+MTrevoux@users.noreply.github.com>
2020-10-04 13:55:30 +0200

    Update temperature.py

--- petoolbox/pressure-temperature/temperature.py
+++ petoolbox/pressure-temperature/temperature.py
@@ -11,25 +11,25 @@ class Temperature(object):
         r : rankine
         k : kelvin
     """
-    unit_list=['c','f','r','k']
+    unit_list=['c', 'f', 'r', 'k']
     
     transfer_matrix_coeff = array([
-                [1,1.8,1.8,1],
-                [1/1.8,1,1,1/1.8],
-                [1/1.8,1,1,1/1.8],
-                [1,1.8,1.8,1]
+                [ 1, 1.8, 1.8, 1 ],
+                [ 1/1.8, 1, 1, 1/1.8 ],
+                [ 1/1.8 ,1 ,1 ,1/1.8 ],
+                [ 1,1.8,1.8,1]
     ])
     transfer_matrix_const = array([
-                [0,0,0,0],
-                [-32,0,0,-32],
-                [-459.67-32,0,0,-459.67-32],
-                [0,-273.15,-273.15,0]
+                [ 0, 0, 0, 0],
+                [-32 ,0 ,0 ,-32 ],
+                [-459.67-32 ,0 ,0 ,-459.67-32 ],
+                [0 ,-273.15 ,-273.15 ,0 ]
     ])
     transfer_matrix_const2 = array([
-                [0,32,459.67+32,273.15],
-                [0,0,459.67,273.15],
-                [0,-459.67,0,273.15],
-                [-273.15,32,32+459.67,0]
+                [0,32, 459.67+32 ,273.15 ],
+                [0, 0, 459.67 ,273.15 ],
+                [0, -459.67, 0, 273.15],
+                [-273.15, 32, 32+459.67, 0]
     ])
     
     def __init__(self, value, reference):
@@ -44,7 +44,8 @@ class Temperature(object):
         if new_ref in Temperature.unit_list:
             i = Temperature.unit_list.index(self.reference)
             j = Temperature.unit_list.index(new_ref)
-            Result = (self.value + Temperature.transfer_matrix_const[i]) * Temperature.transfer_matrix_coeff[i] + Temperature.transfer_matrix_const2[i]
+            Result = (self.value + Temperature.transfer_matrix_const[i]) * Temperature.transfer_matrix_coeff[i]
+                    + Temperature.transfer_matrix_const2[i]
             return Result[j]
         else:
             return f"wrong reference, it must be in {Temperature.unit_list}"
